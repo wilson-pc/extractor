39ab1df48187306764f17e6e6de631ae4fb8c856
Wilver Hidalgo <wilver.hidalgo@believesol.com>
2025-06-28 11:11:36 -0400

    no se

--- src/todo/donghualife.ts
+++ src/todo/donghualife.ts
@@ -15,18 +15,27 @@ export async function donghualifeTodo(
   let title = ''
   const capitulos: Link[] = []
   let full: any[] = []
-  const { data } = await axios.get(link)
-  const $ = cheerio.load(data)
+  const loops = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+  ]
+  const link2 = link.split('?page')[0]
+  for (const page of loops) {
+    const { data } = await axios.get(`${link2}?page=${page}`)
+    const $ = cheerio.load(data)
 
-  const html = $('.view-content tbody a')
-  title = $('.titulo h2 a').first().text().replace(/\\n/g, '').trim()
+    const html = $('.view-content tbody a')
+    title = $('.titulo h2 a').first().text().replace(/\\n/g, '').trim()
 
-  html.each((i, elem) => {
-    //  console.log(elem.children[0])
-    const urld= elem.attribs.href
-    const tittle=elem.children[0].data
-    capitulos.push({ url:"https://donghualife.com"+urld, title: tittle })
-  })
+    html.each((i, elem) => {
+      //  console.log(elem.children[0])
+      const urld = elem.attribs.href
+      const tittle = elem.children[0].data
+      capitulos.push({ url: 'https://donghualife.com' + urld, title: tittle })
+    })
+    if (html.length === 0) {
+      break
+    }
+  }
   if (!links) {
     for (const iterator of capitulos.reverse().slice(salt)) {
       try {
